291e5991b611906ceb402fa25acfd7541dd8a136
Mateo Cisneros <138953180+mateo2408@users.noreply.github.com>
2025-07-04 21:02:35 -0500

    Cambios en PersonRepository.cs

--- People/PersonRepository.cs
+++ People/PersonRepository.cs
@@ -1,4 +1,7 @@
-﻿using People.Models;
+﻿using System;
+using System.Collections.Generic;
+using People.Models;
+using SQLite;
 
 namespace People;
 
@@ -9,10 +12,15 @@ public class PersonRepository
     public string StatusMessage { get; set; }
 
     // TODO: Add variable for the SQLite connection
-
+    
+    private SQLiteConnection conn;
     private void Init()
     {
-        // TODO: Add code to initialize the repository         
+        if (conn != null)
+            return;
+
+        conn = new SQLiteConnection(_dbPath);
+        conn.CreateTable<Person>();
     }
 
     public PersonRepository(string dbPath)
@@ -25,14 +33,13 @@ public class PersonRepository
         int result = 0;
         try
         {
-            // TODO: Call Init()
+            Init();
 
             // basic validation to ensure a name was entered
             if (string.IsNullOrEmpty(name))
                 throw new Exception("Valid name required");
 
-            // TODO: Insert the new person into the database
-            result = 0;
+            result = conn.Insert(new Person { Name = name });
 
             StatusMessage = string.Format("{0} record(s) added (Name: {1})", result, name);
         }
@@ -45,10 +52,10 @@ public class PersonRepository
 
     public List<Person> GetAllPeople()
     {
-        // TODO: Init then retrieve a list of Person objects from the database into a list
         try
         {
-            
+            Init();
+            return conn.Table<Person>().ToList();
         }
         catch (Exception ex)
         {
